33df0b8f7d6e4963ffff01b3c5bb57802d1c81e6
Jeremy Parker <github@jeremyparker.org>
2024-03-23 14:42:21 -0400

    users can register, login, and logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,28 @@
     <title>Connections Admin Portal</title>
 </head>
 <body>
-<div style="border: 3px solid black;">
-<h2>Register</h2>
-<form action="/register" method="POST">
-    @csrf
-    <input name="name" type="text" placeholder="name">
-    <input name="email" type="text" placeholder="email">
-    <input name="password" type="password" placeholder="password">
-    <button>Register</button>
-</form>
-
-</div>
+@auth
+        <form action="/logout" method="POST">
+            @csrf
+            <button>Logout</button>
+        </form>
+@else
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="text" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name">
+            <input name="loginpassword" type="password" placeholder="password">
+            <button>Log in</button>
+        </form>
+@endauth
 </body>
 </html>
