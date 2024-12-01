3cbeb8d940db1b26defe385bc2d9123461546ccc
Jeremy Parker <github@jeremyparker.org>
2024-12-01 18:59:27 -0500

    add categoryWord model

--- resources/views/livewire/word-form.blade.php
+++ resources/views/livewire/word-form.blade.php
@@ -1,15 +1,18 @@
 <div>
-<form wire:submit.prevent="saveWord">
-    @csrf
-    @method('PUT')
-    <input type="text" name="text" placeholder="New Word" wire:model="text" value="{{ $text }}">
-    <label for="isTopical">Is Topical:</label>
-    <input type="checkbox" id="isTopical" name="isTopical" wire:model="isTopical" value="{{ $isTopical }}">
-    <button type="submit">Save</button>
-</form>
+    <form wire:submit.prevent="saveWord">
+        @csrf
+        @method('PUT')
+        <input type="text" name="text" placeholder="New Word" wire:model="text" value="{{ $text }}">
+        <label for="isTopical">Is Topical:</label>
+        <input type="checkbox" id="isTopical" name="isTopical" wire:model="isTopical" value="{{ $isTopical }}">
+        <button type="submit">Save</button>
+        <span wire:loading>Saving...</span>
+    </form>
 
-{{-- <form action="/word/{{$word->id}}/category_word" method="POST">
-    <livewire:categories-combobox name="category" label="Add A Category" placeholder="Start typing" model="categories">
-    <button>Add a category</button>
-</form> --}}
+    <form action="/word/{{ $word->id }}/category_word" method="POST">
+        <button>Add to a category</button>
+    </form>
+
+    {{-- TODO: list of categories component --}}
+
 </div>
