16f85775be669269b0f870bff35663e8263c082b
Jeremy Parker <github@jeremyparker.org>
2025-01-07 22:35:39 -0500

    extend so logins last two months

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 60 * 24 * 60), // about two months
 
     'expire_on_close' => false,
 
